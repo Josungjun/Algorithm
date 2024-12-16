d8a871811574b6d4f86dbe6ba103d546e991c943
Sungjun Jo <66383669+Josungjun@users.noreply.github.com>
2024-12-16 14:19:18 +0900

    [고양이] - 단순구현

---
+++ 0x01/C++/[Bronze5] 1017.cpp
@@ -0,0 +1,16 @@
+/*
+1초 -> 3~5억
+256mb -> 6000만개의 int
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "\\    /\\" << '\n';
+    cout << " )  ( ')" << '\n';
+    cout << "(  /  )" << '\n';
+    cout << " \\(__)|" << '\n';
+}
